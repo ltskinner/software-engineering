bc0a1378bf56588f5b456e9cd466a982b037ef53
unknown <ltskinner@crimson.ua.edu>
2019-12-03 19:39:23 -0700

    consolidated classes + inheritance

--- python/classes/classes_vs_functions.py
+++
@@ -1,9 +0,0 @@
-
-"""
-Two schools of thought on this one:
-    1) Classes when global like variables are needed
-    2) Classes when you need methods to modify an object and its intert data
-        As opposed to a function that completes a task for arbitrary data type
-"""
-
-
