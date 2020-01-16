f2e3f94048b591340e15d9ba07bb74ad0ba2ad60
unknown <ltskinner@crimson.ua.edu>
2020-01-15 20:36:20 -0700

    tinkered with imports order

--- quick_reference/Example-Project/example_project/main_app.py
+++ quick_reference/Example-Project/example_project/main_app.py
@@ -1,11 +1,12 @@
 
 import dash
-from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_bootstrap_components as dbc
 import dash_table
 import dash_daq as daq
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+
 
 
 external_stylesheets = [
