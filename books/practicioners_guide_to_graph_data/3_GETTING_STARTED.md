44d15c7da519962d7d8d861ded9e459bd4c6c48e
ltskinner <33165286+ltskinner@users.noreply.github.com>
2023-12-22 11:40:58 -0500

    script to populate c360 db

--- books/practicioners_guide_to_graph_data/3_GETTING_STARTED.md
+++ books/practicioners_guide_to_graph_data/3_GETTING_STARTED.md
@@ -15,3 +15,109 @@ Goal is to introduce the minimum needed to understand the complexities of using 
 ### Data Models
 
 ### Relational Implementation
+
+### Example C360 Queries
+
+Which credit cards does this customer use?
+
+```sql
+SELECT
+  Customers.customer_id,
+  Customers.name,
+  CreditCards.cc_num,
+  CreditCards.created_date
+FROM
+  Customers
+LEFT JOIN
+  CreditCards
+  ON
+  (Customers.customer_id = CreditCards.customer_id)
+WHERE
+  Customers.customer_id = 'customer_0';
+```
+
+Which accounts does this customer own?
+
+```sql
+SELECT
+  Customers.customer_id,
+  Customers.name,
+  Accounts.acct_id
+  Accounts.created_date
+FROM
+  Customers
+LEFT JOIN
+  Owns
+  ON
+  (Customers.customer_id = Owns.customer_id)
+  LEFT JOIN
+    Accounts
+    ON
+    (Accounts.acct_id = Owns.acct_id)
+WHERE
+  Customers.customer_id = 'customer_0';
+```
+
+Which loans does this customer owe?
+
+```sql
+SELECT
+  Customers.customer_id,
+  Customers.name,
+  Loans.loan_id,
+  Loans.created_date
+FROM
+  Customers
+  LEFT JOIN
+    Owes
+    ON
+    (Customers.customer_id = Owes.customer_id)
+    LEFT JOIN
+      Loans
+      ON
+      (Loans.loan_id = Owes.loan_id)
+WHERE
+  Customers.customer_id = '4';
+```
+
+What do we know about this customer?
+
+```sql
+SELECT
+  Customers.customer_id,
+  Customers.name,
+  Accounts.acct_id,
+  Accounts.created_date,
+  Loans.loan_id,
+  Loans.created_date,
+  CreditCards.cc_num,
+  CreditCards.created_date
+FROM
+  Customers
+LEFT JOIN
+  Owns
+  ON
+  (Customers.customer_id = Owns.customer_id)
+LEFT JOIN
+  Accounts
+  ON
+  (Accounts.acct_id = Owns.acct_id)
+LEFT JOIN
+  Owes
+  ON
+  (Customers.customer_id = Owes.customer_id)
+LEFT JOIN
+  Loans
+  ON
+  (Loans.loan_id = Owes.loan_id)
+LEFT JOIN
+  CreditCards
+  ON
+  (Customers.customer_id = CreditCards.customer_id)
+WHERE
+ Customers.customer_id = 'customer_0';
+```
+
+## Implementing a C360 Application in a Graph System
+
+### Creating your graphs schema
---
+++ books/practicioners_guide_to_graph_data/c360.groovy
@@ -0,0 +1,171 @@
+
+// Vertexes
+schema.vertexLabel("Customer").
+    ifNotExists().
+    partitionBy("customer_id", Text).
+    property("name", Text).
+    create();
+
+schema.vertexLabel("Account").
+    ifNotExists().
+    partitionBy("acct_id", Text).
+    create();
+
+schema.vertexLabel("Loan").
+    ifNotExists().
+    partitionBy("loan_id", Text).
+    create();
+
+schema.vertexLabel("CreditCard").
+    ifNotExists().
+    partitionBy("cc_num", Text).
+    create();
+
+// Edges
+
+schema.edgeLabel("owes").
+    ifNotExists().
+    from("Customer").
+    to("Loan").
+    create();
+
+schema.edgeLabel("uses").
+    ifNotExists().
+    from("Customer").
+    to("CreditCard").
+    create();
+
+schema.edgeLabel("owns").
+    ifNotExists().
+    from("Customer").
+    to("Account").
+    property("role", Text).
+    create();
+
+// Inserting data
+
+michael = g.addV("Customer").
+    property("customer_id", "customer_0").
+    property("name", "Michael").
+    next();
+
+acct_14 = g.addV("Account").
+    property("acct_id", "acct_14").
+    next();
+
+loan_32 = g.addV("Loan").
+    property("loan_id", "loan_32").
+    next();
+
+cc_17 = g.addV("CreditCard").
+    property("cc_num", "cc_17").
+    next();
+
+
+
+g.addE("owns").
+    from(michael).
+    to(acct_14).
+    property("role", "primary").
+    next();
+
+g.addE("owes").
+    from(michael).
+    to(loan_32).
+    next();
+
+g.addE("uses").
+    from(michael).
+    to(cc_17).
+    next();
+
+maria = g.addV("Customer").
+    property("customer_id", "customer_1").
+    property("name", "Maria").
+    next();
+
+g.addE("owns").
+    from(maria).
+    to(acct_14).
+    property("role", "limited").
+    next();
+
+// Data Insertion for Rashika
+rashika = g.addV("Customer").
+    property("customer_id", "customer_2").
+    property("name", "Rashika").
+    next();
+
+acct_5 = g.addV("Account").
+    property("acct_id", "acct_5").
+    next();
+
+cc_32 = g.addV("CreditCard").
+    property("cc_num", "cc_32").
+    next();
+
+
+g.addE("owns").
+    from(rashika).
+    to(acct_5).
+    property("role", "primary").
+    next();
+
+g.addE("uses").
+    from(rashika).
+    to(cc_32).
+    next();
+
+
+// Data Insertion for Jamie
+jamie = g.addV("Customer").
+    property("customer_id", "customer_3").
+    property("name", "Jamie").
+    next();
+
+acct+0 = g.addV("Account").
+    property("acct_id", "acct_0").
+    next();
+
+loan_18 = g.addV("Loan").
+    property("loan_id", "loan_18").
+    next();
+
+g.addE("owns").
+    from(jamie).
+    to(acct_0).
+    property("role", "primary").
+    next();
+
+g.addE("owes").
+    from(jamie).
+    to(loan_18).
+    next();
+
+
+// Data Insertion for Aaliyah
+
+aaliyah = g.addV("Customer").
+    property("customer_id", "customer_4").
+    property("name", "Aaliyah").
+    next();
+
+loan_80 - g.addV("Loan").
+    property("loan_id", "loan_80").
+    next();
+
+g.addE("owns").
+    from(aaliyah).
+    to(acct_0).
+    property("role", "primary").
+    next();
+
+g.addE("owe").
+    from(aaliyah).
+    to(loan_80).
+    next();
+
+g.addE("owes").
+    from(aaliyah).
+    to(loan_18).
+    next();
